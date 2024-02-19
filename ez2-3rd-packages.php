fb4f66e8fc89eec510787c5ff13720be7d3982be
Richer Yang <fantasyworldidvtw@gmail.com>
2024-02-19 15:09:19 +0800

    更新 composer

--- ez2-3rd-packages.php
+++ ez2-3rd-packages.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Ez2 資源包
- * Version: 1.1.11
+ * Version: 1.1.12
  * Author: Ez2.SHOP
  * Requires at least: 6.2
  * Requires PHP: 8.0
@@ -9,7 +9,7 @@
  * License: GPL v2 or later
  **/
 
-define('EZ2_3P_VERSION', '1.1.11');
+define('EZ2_3P_VERSION', '1.1.12');
 define('EZ2_3P_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('EZ2_3P_PLUGIN_URL', plugin_dir_url(__FILE__));
 
